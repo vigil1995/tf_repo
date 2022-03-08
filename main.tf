f5c565fff90520d8dfedd227f35b7aa8e38d0de0
vigil1995 <vigiljoshy@gmail.com>
2022-03-08 12:33:56 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+    
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstorageappdevops"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "Central India"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "binarythistlewa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "binarythistle/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
